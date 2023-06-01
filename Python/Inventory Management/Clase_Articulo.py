853d3b575704690443cb4669fbece0d0e0dca406
Carlos Jimenez <carlosmjimenezg@gmail.com>
2023-05-31 21:56:06 -0400

    Delete of the all file

--- Python/Inventory Management/Clase_Articulo.py
+++
@@ -1,9 +0,0 @@
-class Articulo:
-
-    def __init__(self, codigo, nombre, marca, cantidad, precio, fecha):
-        self.codigo = codigo
-        self.nombre = nombre
-        self.marca = marca
-        self.cantidad = cantidad
-        self.precio = precio
-        self.fecha = fecha
